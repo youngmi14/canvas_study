1c305a15866c88fbcf6fe7e32e5a363f6a8012b2
youngmi <wjdym14@gmail.com>
2021-05-02 19:15:12 +0900

    canvas_0430

--- canvas_04/canvas_0428/canvas_0428.js
+++ canvas_04/canvas_0428/canvas_0428.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext('2d');
 
 ctx.moveTo(20, 20);
 ctx.lineTo(150, 20);
-// arcTo(곡선바깥쪽x, 곡선바깥쪽y, 두번째x, 두번째y, 반지름)
+// arcTo(곡선원바깥쪽x, 곡선원바깥쪽y, 두번째x, 두번째y, 반지름)
 ctx.arcTo(170, 20, 170, 40, 20);
 ctx.lineTo(170, 80);
 ctx.arcTo(170, 100, 190, 100, 20);
