5932a0ba81cd0cb1dd8ffecd57bc3762269b5dc8
youngmi <61876490+youngmi14@users.noreply.github.com>
2021-05-10 16:58:21 +0900

    canvas_0510

---
+++ canvas_05/canvas_0510/0510.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let stars = 500;
+let colorRange = [0, 60, 240];
+
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+for (var i = 0; i < stars; i++) {
+    var x = Math.random() * canvas.offsetWidth;
+    var y = Math.random() * canvas.offsetHeight;
+    var radius = Math.random() * 1.5;
+    var hue = colorRange[getRandom(0, colorRange.length -1)];
+    var sat = getRandom(50, 100);
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 360);
+    ctx.fillStyle = 'hsl(' +hue + ', '+sat+'%, 88%)';
+    ctx.fill();
+}
